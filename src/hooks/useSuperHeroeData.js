c5ea7f0d9ea4bee034bf36cae948bc909ccf2e2d
Magomed Shamaev <ms77grz@pop-os.localdomain>
2022-05-22 13:16:42 +0300

    Axios Interceptor

--- src/hooks/useSuperHeroeData.js
+++ src/hooks/useSuperHeroeData.js
@@ -1,15 +1,17 @@
-import axios from 'axios';
+// import axios from 'axios';
 import { useQuery, useQueryClient } from 'react-query';
+import { request } from '../utils/axios-utils';
 
 const fetchSuperHero = ({ queryKey }) => {
   const heroId = queryKey[1];
-  return axios.get(`http://localhost:4000/superheroes/${heroId}`);
+  return request({ url: `http://localhost:4000/superheroes/${heroId}` });
 };
 
 // export const useSuperHeroeData = heroId => {
 //   return useQuery(['super-hero', heroId], fetchSuperHero);
 // };
 
+// initial query data
 export const useSuperHeroeData = heroId => {
   const queryClient = useQueryClient();
   return useQuery(['super-hero', heroId], fetchSuperHero, {
--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
 import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { request } from '../utils/axios-utils';
 
-const fetchSuperHeroes = () => axios.get('http://localhost:4000/superheroes');
+const fetchSuperHeroes = () => request({ url: '/superheroes' });
 
 const addSuperHero = hero =>
-  axios.post('http://localhost:4000/superheroes', hero);
+  request({ url: '/superheroes', method: 'post', data: hero });
 
 export const useSuperHeroesData = params =>
   useQuery('super-heroes', fetchSuperHeroes, params);
